939f87b054bff04c26831c45151e0d1fba4749fc
Rohit More <cserohitmore@gmail.com>
2023-07-07 11:57:36 +0530

    p

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        bodyFont : ["Montserrat","sans-serif"],
-        titleFont : ["Inter","sans-serif"],
+        bodyFont: ["Montserrat", "sans-serif"],
+        titleFont: ["Inter", "sans-serif"],
       },
-      colors:  {
-        'main-1': '#363636',
-        'main-2': '#7c3aed',
+      colors: {
+        "main-1": "#1A242A",
+        "main-2": "#00A5FF",
       },
       screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-      }
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
-}
-
+};
